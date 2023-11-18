e892686e0b4e8b7215574dffc9383a5dd5c8a6b6
Alexander Grund <Flamefire@users.noreply.github.com>
2023-11-18 17:01:32 +0100

    Add option to skip patching to build.sh and remove `--no-*` style

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -6,13 +6,16 @@ while (($# > 0)); do
     case "$1" in
         --clean)
             export CLEAN_BUILD=1;;
-        --skip-extract|--noextract|--no-extract)
+        --skip-extract)
             export SKIP_EXTRACT=1;;
-        --skip-lfs|--nolfs|--no-lfs)
+        --skip-lfs)
             export CHECK_LFS=0;;
+        --skip-patch)
+            export SKIP_PATCH=1;;
         --fast)
             export SKIP_EXTRACT=1
             export CHECK_LFS=0
+            export SKIP_PATCH=1
             export CHECK_KERNEL_CFG=0;;
         *)
             echo "Invalid option: $@" >&2; exit 1;;
