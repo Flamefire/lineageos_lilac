16b72cb12c463405fc3594a5e41726d8e92189a5
Alexander Grund <Flamefire@users.noreply.github.com>
2025-02-19 18:34:37 +0100

    Minor reformat

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -14,9 +14,9 @@ while (($# > 0)); do
             export SKIP_PATCH=1;;
         --skip-kernel-cfg)
             export CHECK_KERNEL_CFG=0;;
-	--skip-tidy)
-	    export WITH_TIDY=0
-	    ;;
+        --skip-tidy)
+            export WITH_TIDY=0
+            ;;
         --fast)
             export SKIP_EXTRACT=1
             export CHECK_LFS=0
@@ -24,12 +24,12 @@ while (($# > 0)); do
             export CHECK_KERNEL_CFG=0
             export WITH_TIDY=0
             ;;
-	--help|-h)
-	    echo "Usage: $0 [--clean] [--skip-extract] [--skip-lfs] [--skip-patch] [--skip-kernel-cfg] [--skip-tidy]"
-	    echo "--fast combines --skip-extract --skip-lfs --skip-patch --skip-kernel-cfg --skip-tidy"
-	    exit 0;;
+        --help|-h)
+            echo "Usage: $0 [--clean] [--skip-extract] [--skip-lfs] [--skip-patch] [--skip-kernel-cfg] [--skip-tidy]"
+            echo "--fast combines --skip-extract --skip-lfs --skip-patch --skip-kernel-cfg --skip-tidy"
+            exit 0;;
         *)
-            echo "Invalid option: $@" >&2; exit 1;;
+            echo "Invalid option: $*" >&2; exit 1;;
     esac
     shift
 done
