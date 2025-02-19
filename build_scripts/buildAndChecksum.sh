95bf4f994eb32301e8dfb5d9bb64e9b126fd9c0c
Alexander Grund <Flamefire@users.noreply.github.com>
2025-02-19 18:35:58 +0100

    Improved status output of build script

--- build_scripts/buildAndChecksum.sh
+++ build_scripts/buildAndChecksum.sh
@@ -2,7 +2,6 @@ set -eu
 
 source "$(dirname "${BASH_SOURCE[0]}")/setup.sh"
 
-: "${num_procs:=$(nproc)}"
 GREEN='\033[0;32m'
 LGREEN='\033[1;32m'
 RED='\033[0;31m'
@@ -11,6 +10,8 @@ NC='\033[0m'
 
 targetFileName="lineage-$(get_build_var LINEAGE_VERSION).zip"
 targetFile="$ANDROID_PRODUCT_OUT/$targetFileName"
+
+echo -e "${GREEN}Building ${YELLOW}${targetFile}${NC}"
 
 function is_hardlink {
     [[ $(stat --printf '%h' "$1") != 1 ]]
@@ -22,11 +23,11 @@ if [ -e "$targetFile" ]; then
     for f in "$targetFile"*; do
         old_name="$(basename "$f")"
         new_name="${old_name/.zip/.bak.zip}"
-    if is_hardlink "$f"; then
-        cp -a "$f" "$(dirname "$f")/$new_name"
-    else
-        mv "$f" "$(dirname "$f")/$new_name"
-    fi
+        if is_hardlink "$f"; then
+            cp -a "$f" "$(dirname "$f")/$new_name"
+        else
+            mv "$f" "$(dirname "$f")/$new_name"
+        fi
     done
 fi
 
@@ -43,9 +44,14 @@ if mka bacon; then
         echo -e "${RED}Found git LFS files in $outDir!${NC}" && false
     fi
 
-    otaFile="$ANDROID_PRODUCT_OUT/$(get_build_var TARGET_PRODUCT)-ota-"*.zip
-    [ ! -e "$otaFile" ] || rm "$otaFile"
+    otaFiles=( "$ANDROID_PRODUCT_OUT/$(get_build_var TARGET_PRODUCT)-ota-"*.zip )
+    otaFile=${otaFiles[0]}
+    if [ -e "$otaFile" ]; then
+        echo -e "${YELLOW}Removing OTA file $otaFile...${NC}"
+        rm "$otaFile"
+    fi
     if is_hardlink "$targetFile"; then
+        echo -e "${YELLOW}Un-hardlink $targetFile...${NC}"
         tmpFile="$(mktemp -p /dev/shm)"
         cp -af "$targetFile" "$tmpFile"
         mv -f "$tmpFile" "$targetFile"
@@ -62,6 +68,7 @@ if mka bacon; then
     fi
     (cd "$ANDROID_PRODUCT_OUT" && md5sum lineage-*.zip)
     mv "$targetFile"* "$OUT_DIR_COMMON_BASE"
+    echo "Moved $(md5sum "$OUT_DIR_COMMON_BASE/$targetFileName")"
 else
     echo "Failed to build" && false
 fi
