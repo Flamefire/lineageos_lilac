e1e00329fbac117d240406bdec3a11036aaf1ae3
Alexander Grund <Flamefire@users.noreply.github.com>
2025-02-22 12:58:01 +0100

    Add FAKE_PACKAGE_SIGNATURE for MicroG
    At least 17.1 bootloops after a clean flash without this.
    19.1 might although not easily reproducible.

--- build_scripts/buildMicroG.sh
+++ build_scripts/buildMicroG.sh
@@ -14,13 +14,17 @@ if [ ! -e "vendor/partner_gms/products/gms.mk" ]; then
   exit 1
 fi
 
+makefile_containing_version="vendor/lineage/config/version.mk"
+if [ ! -f "$makefile_containing_version" ]; then
+  makefile_containing_version="vendor/lineage/config/common.mk"
+fi
+permissionManifestFile=frameworks/base/core/res/AndroidManifest.xml
+
 function reset_patches {
   # Remove previous changes (if they exist)
-  for path in "vendor/lineage"; do
-    if [ -d "$path" ]; then
-      printStatus "Removing changes to $path"
-      (cd "$path"; git reset -q --hard; git clean -q -fd)
-    fi
+  for path in "$makefile_containing_version" "$permissionManifestFile"; do
+    printStatus "Removing changes to $path"
+    git -C "$(dirname "$path")" checkout "$(basename "$path")"
   done
 }
 
@@ -28,10 +32,13 @@ reset_patches
 # Ensure source dir is clean on exit
 trap 'reset_patches' EXIT
 
-makefile_containing_version="vendor/lineage/config/common.mk"
-if [ -f "vendor/lineage/config/version.mk" ]; then
-  makefile_containing_version="vendor/lineage/config/version.mk"
-fi
+# Ensure FAKE_PACKAGE_SIGNATURE permission is recognized even though it isn't actually implemented.
+# It is still requested by MicroG (granted by privapp-permissions-xml) and might be verified on first boot.
+# If it isn't known the app will be terminated preventing the system from booting.
+permission='<permission android:name="android.permission.FAKE_PACKAGE_SIGNATURE" android:protectionLevel="signature|privileged" />'
+printStatus "Adding FAKE_PACKAGE_SIGNATURE to $permissionManifestFile"
+sed -i "0,\#^\s*<permission#s##<!-- @hide --> $permission\n&#" "$permissionManifestFile"
+
 printStatus "Patching build type check in $makefile_containing_version"
 sed -i "/\$(filter .*\$(LINEAGE_BUILDTYPE)/,/endif/d" "$makefile_containing_version"
 
