6db3dee2723522c86c73cf52dd6e058470244635
Alexander Grund <Flamefire@users.noreply.github.com>
2025-12-28 14:21:19 +0100

    Make autopif.sh more reliable and improve logging
    Update device selection logic to retry on missing links.

--- tools/autopif.sh
+++ tools/autopif.sh
@@ -6,14 +6,14 @@
 set -euo pipefail
 
 function error {
-  echo "$@"
+  echo "$@" >&2
   exit 1
 }
 
 function download {
   url=$1
   file=${2:-}
-  echo "- Downloading $url ..." >&2;
+  echo "- Downloading $url ..." >&2
   if [[ -n "$file" ]]; then
     curl --silent --show-error --location --output "${file}" "${url}" || error "Failed"
   else
@@ -27,16 +27,16 @@ else
     { mkdir -p "$1" && cd "$1"; } || error "Failed to change to specified folder $1"
 fi
 
-# Get latest Pixel Beta information
+echo "- Get latest Pixel Beta information"
 download https://developer.android.com/about/versions PIXEL_VERSIONS_HTML
 BETA_URL=$(grep -o 'https://developer.android.com/about/versions/.*[0-9]"' PIXEL_VERSIONS_HTML | sort -ru | cut -d\" -f1 | head -n1)
 download "$BETA_URL" PIXEL_LATEST_HTML
 
-# Get OTA information
+echo "- Get OTA information"
 OTA_URL="https://developer.android.com$(grep -o 'href=".*download-ota.*"' PIXEL_LATEST_HTML | grep 'qpr' | cut -d\" -f2 | head -n1)"
 download "$OTA_URL" PIXEL_OTA_HTML
 
-# Extract device information
+echo "- Extract device information"
 MODEL_LIST=$(grep -A1 'tr id=' PIXEL_OTA_HTML | grep 'td' | sed 's;.*<td>\(.*\)</td>;\1;')
 PRODUCT_LIST=$(grep -o 'tr id="[^"]*"' PIXEL_OTA_HTML | awk -F\" '{print $2 "_beta"}')
 OTA_LIST=$(grep 'ota/.*_beta' PIXEL_OTA_HTML | cut -d\" -f2)
@@ -45,28 +45,35 @@ OTA_LIST=$(grep 'ota/.*_beta' PIXEL_OTA_HTML | cut -d\" -f2)
 echo "- Selecting Pixel Beta device ..."
 count=$(echo "$MODEL_LIST" | wc -l)
 count2=$(echo "$PRODUCT_LIST" | wc -l)
-if ((count != count2)); then
-  echo "Warning: MODEL_LIST and PRODUCT_LIST have different lengths, using Pixel 6 fallback"
-  MODEL="Pixel 6"
-  PRODUCT="oriole_beta"
-  OTA_LINK_CHECK="$(echo "$OTA_LIST" | grep "$PRODUCT")"
-else
-  rand_index=$(( $$ % count + 1 ))
-  MODEL=$(echo "$MODEL_LIST" | sed -n "${rand_index}p")
-  PRODUCT=$(echo "$PRODUCT_LIST" | sed -n "${rand_index}p")
-  OTA_LINK_CHECK=$(echo "$OTA_LIST" | sed -n "${rand_index}p")
-fi
-echo "$MODEL ($PRODUCT)"
+for tries in $(seq 10); do
+  if ((count != count2)); then
+    echo "WARNING: MODEL_LIST and PRODUCT_LIST have different lengths, using Pixel 6 fallback"
+    MODEL="Pixel 6"
+    PRODUCT="oriole_beta"
+  else
+    rand_index=$(( $$ % count + 1 ))
+    MODEL=$(echo "$MODEL_LIST" | sed -n "${rand_index}p")
+    PRODUCT=$(echo "$PRODUCT_LIST" | sed -n "${rand_index}p")
+  fi
+  echo "- Selected: $MODEL ($PRODUCT)."
 
-# Get device fingerprint and security patch from OTA metadata
-if ! OTA_LINK="$(echo "$OTA_LIST" | grep "$PRODUCT")"; then
-  echo "ERROR - Faild to find '$PRODUCT' in OTA list: $OTA_LIST"
+  echo "- Get device fingerprint and security patch from OTA metadata"
+  if OTA_LINK="$(echo "$OTA_LIST" | grep "$PRODUCT")"; then
+    break;
+  else
+    echo "ERROR - Failed to find '$PRODUCT' in OTA list:"
+    ((tries != 1 )) || echo "$OTA_LIST"
+    OTA_LINK=""
+  fi
+done
+
+if [[ -z $OTA_LINK ]]; then
+  echo "ERROR - Failed to find valid download link"
   exit 1
 fi
-[[ "$OTA_LINK" == "$OTA_LINK_CHECK" ]] || echo "WARNING: Different links: '$OTA_LINK' != '$OTA_LINK_CHECK'"
-set +o pipefail
-download "$OTA_LINK" | grep -ao '[ -~]\{10,\}' | head -n15 > PIXEL_ZIP_METADATA
-set -o pipefail
+
+echo "- Extracting metdata from OTA ..."
+download "$OTA_LINK" | grep -ao '[ -~]\{10,\}' > PIXEL_ZIP_METADATA
 echo "- Getting fingerprint ..."
 FINGERPRINT="$(grep -am1 'post-build=' PIXEL_ZIP_METADATA | cut -d= -f2)"
 echo "- Getting security level ..."
